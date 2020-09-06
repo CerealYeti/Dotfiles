9eb15e14e6d8b2d1ee0dbd13adcded6cc66e45d7
CerealYeti <geddypence@gmail.com>
2020-09-06 22:57:44 +0000

    quick update to install script

--- new_ubuntu_install.sh
+++ new_ubuntu_install.sh
@@ -22,8 +22,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # cp configs to the correct place
-cp ./.zshrc ~/
-cp ./.tmux.conf ~/
+cp ./.zshrc ../
+cp ./.tmux.conf ../
 
 # install thefuck
 pip3 install thefuck
