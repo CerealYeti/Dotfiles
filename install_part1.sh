dc349642f522c944f188c5af571a52e389d60c33
Geddy <geddypence@gmail.com>
2020-09-11 19:06:51 -0700

    Update and rename new_ubuntu_install.sh to install_part1.sh

--- install_part1.sh
+++ install_part1.sh
@@ -15,17 +15,5 @@ apt-get install -y python3 python3-dev python3-pip python3-setuptools
 
 # install zsh, set default shell, then get oh-my-zsh and download all dependencies and configs
 apt-get install -y zsh
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-apt-get install -y fonts-powerline
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# cp configs to the correct place
-cp ./.zshrc ../
-cp ./.tmux.conf ../
-
-# install thefuck
-pip3 install thefuck
-
-source ~/.zshrc
