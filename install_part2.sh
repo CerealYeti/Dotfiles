f7f400789b0c029bde952a1c65d9685e68f6434d
Geddy <geddypence@gmail.com>
2020-09-11 19:07:11 -0700

    Update install_part2.sh

--- install_part2.sh
+++ install_part2.sh
@@ -1,1 +1,12 @@
+apt-get install -y fonts-powerline
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# cp configs to the correct place
+cp ./.zshrc ../
+cp ./.tmux.conf ../
+
+# install thefuck
+pip3 install thefuck
+
+source ~/.zshrc
