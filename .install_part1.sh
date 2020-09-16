703e2bb8ae142e807bb22be10d3bfb094aface25
CerealYeti <geddypence@gmail.com>
2020-09-16 13:22:05 -0700

    changed the oh-my-zsh install part of the script

--- .install_part1.sh
+++ .install_part1.sh
@@ -20,6 +20,3 @@ apt-get install -y fonts-powerline
 
 pip3 install thefuck
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
