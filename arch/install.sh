b9c3d389f6f792fdedaea0c5f33d59900b31a3c7
Geddy <geddypence@gmail.com>
2021-03-18 18:05:55 -0500

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -7,3 +7,17 @@ fi
 pacman -Syy
 pacman -Syu
 
+xargs -a packages/pacman-install.txt pacman -S --needed --noconfirm
+
+go get -u github.com/cheat/cheat/cmd/cheat
+
+cd /opt
+git clone https://aur.archlinux.org/yay.git
+cd ./yay
+makepkg -si
+
+yay -Syu --devel --timeupdate
+
+xargs -a packages/aur-install.txt yay -S --noconfirm
+
+pacman -Rcs $(pacman -Qdtq)
